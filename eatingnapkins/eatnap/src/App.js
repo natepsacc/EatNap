76cfe1e440c556786e279f425bac7610063b3f7b
Nathan Schmidt <natepsacc@gmail.com>
2022-04-12 19:03:56 -0700

    before im too high

--- eatingnapkins/eatnap/src/App.js
+++ eatingnapkins/eatnap/src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.png';
 import './App.css';
+import post from './post.js';
 
 function read()
 {
+
      var txtFile = new XMLHttpRequest();
      txtFile.open("GET", "https://storageapi2.fleek.co/e7b18505-5349-459d-9fba-f427d40e6079-bucket/text/firstPageDesc.txt", true);
      txtFile.onreadystatechange = function()
@@ -20,20 +22,38 @@ function read()
      txtFile.send(null)
 }
 
+
+function rmbut(){
+
+  var div = document.getElementById('enEnterButton');
+  var enEnter = document.getElementById('enEnter');
+  var desc = document.getElementById("desc");
+  div.parentNode.removeChild(div);
+  desc.style.width = "35vw";
+  enEnter.style.width = "95%";
+  enEnter.style.marginTop = "1vh";
+  enEnter.style.height = "95vh";
+
+  post();
+
+}
+
 function App() {
+  document.getElementById("forestIMG");
  read()
   return (
     <div>
     <div class="bg"></div>
     <div class="bg bg2"></div>
     <div class="bg bg3"></div>
-    <div className="enEnter">
+    <div className="enEnter" id="enEnter">
         <img src={logo} className="EN-logo" alt="logo" />
         <p id="desc">
 
         </p>
-        <div className="enEnterButton">
-         <a href="#">Enter The Digital Gallery</a>
+
+        <div id="enEnterButton" className="enEnterButton">
+         <a href="#" onClick={rmbut} >Enter The Digital Gallery</a>
         </div>
     </div>
     </div>
